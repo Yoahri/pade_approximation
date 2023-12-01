446a54a44536ccfa6b9621153acc8513316eb773
Vitor Brito <vitorperes2002@usp.br>
2023-12-01 13:41:02 -0300

    final commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
 from pade import pade_approximation
-from sympy import symbols, exp, sin, diff, cos
+from sympy import symbols, exp, sin, cos
 from generatepoints import generatepoints
 import matplotlib.pyplot as plt
+
 def main(func):
     print("Padé para a seguinte função:", func)
     while True:
@@ -22,43 +23,51 @@ def main(func):
         except:
             print("Deve ser um valor positivo maior que zero")
             continue
-    
-           
+
     ratio = pade_approximation(m, n, func)
-    points=generatepoints(69)
+    points = generatepoints(69)
     points.sort()
-    resultspade=[]
-    resultfunc=[]
+    resultspade = []
+    resultfunc = []
 
-   
     for _ in points:
-        value=ratio.subs([(x,_)]).evalf()
+        value = ratio.subs([(x, _)]).evalf()
         resultspade.append(value)
-        valuefunc=func.subs([(x,_)]).evalf()
+        valuefunc = func.subs([(x, _)]).evalf()
         resultfunc.append(valuefunc)
-    resultfunc=np.array(resultfunc)
-    resultspade=np.array(resultspade)
-    error=resultfunc-resultspade
+
+    resultfunc = np.array(resultfunc)
+    resultspade = np.array(resultspade)
+    error = resultfunc - resultspade
     norma_maxima = np.max(np.abs(error))
-    norma_erro_l2 =(np.sum(np.array(error, dtype=float)**2))**(1/2)
-    error=np.abs(error)
-    
-    print("Erro médio(soma dos erros em módulo pela quantidade de pontos):", np.mean(np.abs(error)), "\n", "Erro Max:", norma_maxima, "\n", "Norma L2:", norma_erro_l2)
+    norma_erro_l2 = (np.sum(np.array(error, dtype=float) ** 2)) ** (1/2)
+    error = np.abs(error)
+
+    print("Erro médio (soma dos erros em módulo pela quantidade de pontos):", np.mean(np.abs(error)), "\n", "Erro Max:", norma_maxima, "\n", "Norma L2:", norma_erro_l2)
+
     plt.plot(points, resultspade, label='Padé', color='red')
-    plt.plot(points, resultfunc, label='func', color='blue')
+    plt.plot(points, resultfunc, label='Func', color='blue')
+    plt.xlabel('Pontos')
+    plt.ylabel('Valores')
+    plt.legend()
     plt.show()
+
     plt.plot(points, error, label='Erro', color='blue')
+    plt.xlabel('Pontos')
+    plt.ylabel('Erro')
+    plt.legend()
     plt.show()
 
-x= symbols('x', real=True)
+x = symbols('x', real=True)
+
 while True:
-    main(exp(x)*sin(x)*cos(x))
-    y=input("Deseja contunar(s/n)?")
-    if y == "n":
-        break
-while True:
-    main(exp(x)*sin(x)/cos(x)**2)
-    y=input("Deseja continuar(s/n)?")
-    if y == "n":
+    main(exp(x) * sin(x) * cos(x))
+    y = input("Deseja continuar (s/n)?")
+    if y.lower() == "n":
         break
 
+while True:
+    main(exp(x) * sin(x) / cos(x) ** 2)
+    y = input("Deseja continuar (s/n)?")
+    if y.lower() == "n":
+        break
